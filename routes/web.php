80f8610c74bc86434903f1c6a7f543177f439aae
LeoSallu <L.sallustio99@gmail.com>
2023-10-07 14:32:56 +0200

    installed breeze,daisyUi and feat route admin

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    if(auth()->check()){
+        return route('admin.dashboard');
+    }
+    else{
+        return view('auth.login');
+    }
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')
+->prefix('admin')
+->name('admin.')
+->group(function () {
+    Route::get('/dashboard', function () { 
+        return view('admin.dashboard');
+    })->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
